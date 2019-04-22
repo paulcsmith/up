82b9e6e32f00d009173f3847de7886db0923e9f9
Paul Smith <paulcsmith0218@gmail.com>
2019-04-21 22:23:44 -0400

    Ideas

--- src/up/commands/run_in_container.cr
+++ src/up/commands/run_in_container.cr
@@ -8,6 +8,15 @@ class Up::RunInContainer < Up::Command
   end
 
   def call(args)
+    #     project_name=$(basename "$(pwd | tr -d '_ -')")
+    # image_name=${project_name}_${service}_1
+    # docker inspect -f {{.State.Running}} image name
+
+    # docker-compose ps
+    #
+    # Check that all are up (not have "Exit")
+
+    # If so, use `exec` instead of `run --rm`
     Up::RebuildIfChanged.call
     Up::Utils.docker_compose("run --rm #{args.join(" ")}")
   end
